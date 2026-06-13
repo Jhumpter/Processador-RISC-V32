8c2b5a747bc36c31562bde57f25699a70fb4df2d
jhumpter <joaopwii@gmail.com>
2026-06-12 22:28:14 -0300

    feat: controlando a ula

---
+++ src/controle_ula.sv
@@ -0,0 +1,41 @@
+module controle_ula(
+input logic [6:0] funct7,
+input logic [2:0] funct3,
+input logic [1:0] alu_op,
+output logic [3:0] controle_ula
+);
+
+always_comb begin
+	case(alu_op)
+		2'b00:
+			controle_ula = 4'b0010;
+		2'b01:
+			controle_ula = 4'b0110;
+		2'b10:begin
+			case(funct7)
+				7'b0100000:
+					controle_ula = 4'b0110;
+				7'b0000000:begin
+					case (funct3)
+						3'b000:
+							controle_ula = 4'b0010;
+						3'b111:
+							controle_ula = 4'b0000;
+						3'b110:
+							controle_ula = 4'b0001;
+						3'b010:
+							controle_ula = 4'b0111;
+						default:
+							controle_ula = 4'b0000;
+					endcase
+				end
+				default:
+					controle_ula = 4'b0000;
+			endcase
+		end
+		default:
+			controle_ula = 4'b0000;
+	endcase
+end
+
+endmodule
