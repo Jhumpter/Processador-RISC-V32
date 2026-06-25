1f5f079f18503f771995adb12b97053aa8f16d22
Jhumpter <joaopwii@gmail.com>
2026-06-25 01:02:12 -0300

    feat: começando a integrar com o .mif de instruções
    Parece que no teste ele pula especificamente os endereços b e c

---
+++ src/memoria_instrucoes.sv
@@ -0,0 +1,13 @@
+module memoria_instrucoes (
+    input  logic [31:0] endereco,
+    output logic [31:0] instrucao
+);
+
+    // 16384 palavras de 32 bits (igual ao DEPTH do MIF)
+    (* ram_init_file = "programa.mif" *)
+    logic [31:0] mem [0:16383];
+
+    // acesso direto por palavra (MIF já está nesse formato)
+    assign instrucao = mem[endereco[15:2]];
+
+endmodule
